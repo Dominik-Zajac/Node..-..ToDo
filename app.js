9d5b3382dcf5a958032df260308101ac9b594f97
Wyrafinowany <zajac_dominik@onet.pl>
2019-06-12 11:37:28 +0200

    Edit handleCommand

--- app.js
+++ app.js
@@ -15,13 +15,17 @@ const handleCommand = ({
         } else if (add.length < 7) {
             return console.log('Nazwa zadnaia musi miec wiecej niz 6 znakow'.red);
         }
-    }
-    if (remove) {
+    } else if (remove) {
+        if (typeof remove !== 'string' || remove.length < 7) {
+            return console.log('Wpisz nazwe usuwanego zadania!'.red);
+        }
+    } else if (list || list === '') {
 
-    }
-    if (list) {
-
+    } else {
+        console.log(`Nie rozumiem zadania. Wzyj --add'nazwa zadania', --remove='nazwa-zadania' lub opcji '--list'`);
     }
 }
+
+
 
 handleCommand(command)
