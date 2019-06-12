e86ef8b90ed4de3230c008af34f9e61cd35edcf6
Wyrafinowany <zajac_dominik@onet.pl>
2019-06-12 11:30:37 +0200

    Add handleCommand function

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 const parseArgs = require('minimist');
+const colors = require('colors');
 
 const command = parseArgs(process.argv.slice(2, 3));
 delete command._;
-console.log(command)
+
+const handleCommand = ({
+    add,
+    remove,
+    list
+}) => {
+    if (add) {
+        if (typeof add !== 'string') {
+            return console.log('Wpisz nazwe zadania!'.red)
+        } else if (add.length < 7) {
+            return console.log('Nazwa zadnaia musi miec wiecej niz 6 znakow'.red);
+        }
+    }
+    if (remove) {
+
+    }
+    if (list) {
+
+    }
+}
+
+handleCommand(command)
